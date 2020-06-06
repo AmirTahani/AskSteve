84a0c3151c8b074766ef9c3203bdb177842b9190
Amir <jopp@Jopps-MacBook-Pro.local>
2020-06-06 22:02:37 +0430

    add packages + config routes

--- App.js
+++ App.js
@@ -1,26 +1,21 @@
 import * as React from 'react';
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
 import createStore from './src/redux/store';
-import Login from './src/Screens/Auth/Login';
-
-const Stack = createStackNavigator();
+import Routes from './src/navigation/routes';
 
 
 const { store, persistor } = createStore();
-export default function App() {
+
+export default function App(props) {
+  console.log(props);
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Login" component={Login}/>
-          </Stack.Navigator>
-        </NavigationContainer>
+        <Routes/>
       </PersistGate>
     </Provider>
   )
 }
+
