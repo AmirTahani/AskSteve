d18f00a34eecc2f6185b1f0e61011ad0f9581e64
Amir <jopp@Jopps-MacBook-Pro.local>
2020-06-10 17:27:31 +0430

    Empty list component is now using text prop with default value

--- src/Components/kit/EmptyList.js
+++ src/Components/kit/EmptyList.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
-export default function EmptyList() {
+export default function EmptyList(props) {
   return (
-    <Text style={styles.text}>Nothing to show</Text>
+    <Text style={styles.text}>{props.text}</Text>
   );
 }
+
+EmptyList.defaultProps = {
+  text: 'Nothing to Show'
+};
 
 const styles = StyleSheet.create({
   text: {
